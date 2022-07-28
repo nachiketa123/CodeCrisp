930318f9dcd3d4f5f58bca33ea3224661a8eaff1
ayush <ayush.kumar.ak7346@gmail.com>
2022-07-28 19:34:27 +0530

    PostAdd

--- codecrisp-frontend/src/App.js
+++ codecrisp-frontend/src/App.js
@@ -1,11 +1,13 @@
 
 import './App.css';
 import Header from './component/Header';
+import Post from './component/Post';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Post />
     </div>
   );
 }
