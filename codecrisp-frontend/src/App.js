beba15793d8f2f70fb9d63c192434ab415d91054
Nachiketa <nachiketarajput100@gmail.com>
2022-07-31 00:23:22 +0530

    Post component added for the first time, added font-awsome icons in assets

--- codecrisp-frontend/src/App.js
+++ codecrisp-frontend/src/App.js
@@ -1,11 +1,13 @@
 
 import './App.css';
 import Header from './component/Header';
+import PostComponent from './component/post-component/post';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <PostComponent/>
     </div>
   );
 }
