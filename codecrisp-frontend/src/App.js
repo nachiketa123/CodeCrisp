fca9a53dc0241020045fc031078f3b843146e4fe
Luv895 <iamluv895@gmail.com>
2022-08-06 13:59:14 +0530

    changes uploaded

--- codecrisp-frontend/src/App.js
+++ codecrisp-frontend/src/App.js
@@ -1,11 +1,13 @@
 
 import './App.css';
 import Header from './component/Header';
+import CommunityNotification from './component/SideComponent';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <CommunityNotification/>
     </div>
   );
 }
---
+++ codecrisp-frontend/src/component/SideComponent.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './SideComponent.css'
+const CommunityNotification = () => {
+    return ( 
+    <div >
+        <div className='flex-item'>
+            <div className='title'> Community Notification</div>
+            <div className='CommunityNotification' ></div>
+            <div className='title'> Tech News</div>
+            <div className='TechNews' ></div>
+            <div className='title'> Quotes</div>
+            <div className='Quotes' ></div>
+        </div>
+    </div> );
+}
+ 
+export default CommunityNotification;
