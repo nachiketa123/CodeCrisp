9f9586ee6a04b3a8639492709091a17022b10a42
ayush <ayush.kumar.ak7346@gmail.com>
2022-08-06 13:27:57 +0530

    Home Page Development - Login

--- codecrisp-frontend/src/App.js
+++ codecrisp-frontend/src/App.js
@@ -2,29 +2,33 @@
 import './App.css';
 import ChatBox from './component/chat-box-component/ChatBox';
 import Header from './component/Header';
+import HomePage from './component/HomePage/HomePage';
 import AllPosts from './component/post-component/all-posts';
 
 function App() {
   return (
     <div className="App">
+
+      <HomePage />
+
       <div className='app-container'>
         <div className='header'>
-          <Header />
+          {/* <Header /> */}
         </div>
         <div className='feed-body'>
           <div className='chat-box'>
-            <ChatBox />
+            {/* <ChatBox /> */}
           </div>
           <div className='all-posts'>
-            <AllPosts/>
+            {/* <AllPosts/> */}
           </div>
           <div className='notification-panel'>
             {/* Right side notification panel */}
-          </div> 
+          </div>
         </div>
       </div>
-      
-      
+
+
     </div>
   );
 }
--- codecrisp-frontend/src/component/post-component/post.js
+++ codecrisp-frontend/src/component/post-component/post.js
@@ -1,21 +1,21 @@
 import React, { Component } from 'react';
 import './post.css';
-import heart  from '../../assets/fontawesome/svgs/regular/heart.svg';
-import comment  from '../../assets/fontawesome/svgs/regular/comment.svg';
-import share  from '../../assets/fontawesome/svgs/solid/share.svg';
+import heart from '../../assets/fontawesome/svgs/regular/heart.svg';
+import comment from '../../assets/fontawesome/svgs/regular/comment.svg';
+import share from '../../assets/fontawesome/svgs/solid/share.svg';
 
 
 class PostComponent extends Component {
-    state = {  } 
-    render() { 
+    state = {}
+    render() {
         return (
             <div className='my-card container'>
                 <div className='user-details post-common'>
-                    <div className='user-dp-info'> 
+                    <div className='user-dp-info'>
                         <img className='dp-img' src={require('../../assets/images/nach_profile.jpg')} alt="Profile Picture" />
                         <div className='user-info'>
-                            <p style={{fontWeight:'bold',fontSize:'16px',margin:'0'}}>Here goes user name</p>
-                            <p style={{color:'grey',fontSize:'12px',margin:'0'}}>Here goes user location</p>
+                            <p style={{ fontWeight: 'bold', fontSize: '16px', margin: '0' }}>Here goes user name</p>
+                            <p style={{ color: 'grey', fontSize: '12px', margin: '0' }}>Here goes user location</p>
                         </div>
                     </div>
                     <img className='post-menu-img' src={require('../../assets/icons/post_menu.png')} alt="post menu" />
@@ -30,21 +30,21 @@ class PostComponent extends Component {
                         <img className='share-img' src={share} alt="share" />
                     </div>
                     <div className='who-liked-post'>
-                        <p style={{color:'#ffc107',fontWeight:'bold',fontSize:'12px',margin:'0',background:'transparent',margin:'2px 10px 8px 0'}}> someone liked your post</p>
+                        <p style={{ color: '#ffc107', fontWeight: 'bold', fontSize: '12px', margin: '0', background: 'transparent', margin: '2px 10px 8px 0' }}> someone liked your post</p>
                     </div>
                 </div>
                 <div className='username-caption'>
-                    
-                    <p style={{color:'grey',fontSize:'12px',margin:'0'}}>
-                        <h3 style={{color:'white',fontWeight:'bold',fontSize:'12px',margin:'0 10px 0 0',display:'inline'}}>Nachu121</h3>
+
+                    <p style={{ color: 'grey', fontSize: '12px', margin: '0' }}>
+                        <h3 style={{ color: 'white', fontWeight: 'bold', fontSize: '12px', margin: '0 10px 0 0', display: 'inline' }}>Nachu121</h3>
                         This is my caption for the post
-                     </p>
-                </div>  
+                    </p>
+                </div>
                 <div className='post-comments post-common'>
-                    <button style={{color:'white',fontSize:'12px',margin:'0'}} className="secondary">see all comments (11)</button>
+                    <button style={{ color: 'white', fontSize: '12px', margin: '0', background: "transparent" }} className="secondary">see all comments (11)</button>
                     <div class="comment-wrapper">
-                        <img src="img/smile.PNG" class="icon" alt=""/>
-                        <input type="text" class="comment-box" placeholder="Add a comment"/>
+                        <img src="img/smile.PNG" class="icon" alt="" />
+                        <input type="text" class="comment-box" placeholder="Add a comment" />
                         <button class="comment-btn">post</button>
                     </div>
                 </div>
@@ -52,6 +52,6 @@ class PostComponent extends Component {
         );
     }
 }
- 
+
 
 export default PostComponent;
