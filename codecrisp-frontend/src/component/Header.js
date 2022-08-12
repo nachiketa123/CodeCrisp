990f938ae2427b053cddc09c9a6c91e2a254556f
Nachiketa <nachiketarajput100@gmail.com>
2022-08-12 15:47:48 +0530

    Added bell-icon for notification and chat-icon for chat in Header.js and made it responsive

--- codecrisp-frontend/src/component/Header.js
+++ codecrisp-frontend/src/component/Header.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import './Header.css';
-import { FaBars } from 'react-icons/fa';
+import { FaBars, FaRegBell, FaRegComments, FaSearch } from 'react-icons/fa';
 
 function Header() {
     return (
@@ -11,7 +11,6 @@ function Header() {
                         <div className='nav-left'>
                             <img className='logo-image' src={require('../images/logo.png')} />
                             <a className="navbar-brand" href="#"
-                                style={{ fontSize: "30px", marginLeft: "15px" }}
                             >CodeCrisp</a>
                         </div>
 
@@ -19,16 +18,19 @@ function Header() {
                         <div className='search'>
                             <form className="form-inline my-2  searchBar">
                                 <input className="mr-2 searchBar-input" type="search" placeholder="Search Developer" aria-label="Search" />
-                                <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+                                <FaSearch color='seagreen' className="search-icon my-2 my-sm-0" title='search'/>
                             </form>
                         </div>
+
+                        <FaRegBell color='white' className='bell-icon' title='notifications'/>
+                        <FaRegComments color='white' className='chat-icon' title='chat'/>
 
                           {/* Toggle Area */}
                           <button className='navbar-toggler' type="button" data-toggle="collapse" 
                         data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" 
                         aria-expanded="false" aria-label="Toggle navigation">
                             
-                            <FaBars color='white'></FaBars>
+                            <FaBars color='white' className='hamburger-icon'></FaBars>
 
                         </button>
 
