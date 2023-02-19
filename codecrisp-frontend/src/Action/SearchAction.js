adf3b5069854ca831fd951497e6f65388d97ef17
Nachiketa <nachiketarajput100@gmail.com>
2023-02-19 17:31:06 +0530

    Performance optimization for SearchResult, added cancel the request if already there is one request pending

--- codecrisp-frontend/src/Action/SearchAction.js
+++ codecrisp-frontend/src/Action/SearchAction.js
@@ -1,17 +1,30 @@
 import axios from "axios";
 import { SEARCH_RESULT, GET_ERROR } from "./Types";
-
+let cancel = null
 export const searchResult = (userFind) => (dispatch) => {
 
-
-
-    axios.get('/api/searchuser', { params: { searchText: userFind.searchText } }).then(
-        res => {
-            dispatch({ type: SEARCH_RESULT, payload: res.data })
-        }
-    ).catch(
-        err => {
-            dispatch({ type: GET_ERROR, payload: err.response.data })
-        }
-    )
+    if(cancel){
+        cancel()
+    }
+    const source = axios.CancelToken.source()
+    cancel = source.cancel
+    setTimeout(()=>{
+        axios.get('/api/searchuser', { params: { searchText: userFind.searchText }, cancelToken: source.token }).then(
+            res => {
+                console.log('searching..')
+                dispatch({ type: SEARCH_RESULT, payload: res.data })
+            }
+        ).catch(
+            err => {
+                if(axios.isCancel(err)){
+                    //Ignore
+                }
+                else{
+                    dispatch({ type: GET_ERROR, payload: err.response.data })
+                }
+                
+            }
+        )
+    },500)
 }
+    
